e9a1609b617d74550ce1be0033ad833bcb01b015
Luke Lu <chungl7@uci.edu>
2025-10-02 12:56:20 -0700

    second

--- Shape.java
+++ Shape.java
@@ -27,18 +27,115 @@ class Circle extends Shape {
     double radius;
     static final double Pi = 3.14;
 
-    Circle(double newRadius) {super(); radius = newRadius;}
+    public Circle(String name, double newRadius) 
+    {
+        super(name); 
+        radius = newRadius;
+    }
 
-    double area() {return Pi * radius * radius;}
+    double area() 
+    {
+        return Pi * radius * radius;
+    }
 
     void draw()
     {
-        System.out.printf("");
+        System.out.println("   *****   ");
+        System.out.println(" **     ** ");
+        System.out.println("*         *");
+        System.out.println("*         *");
+        System.out.println("*         *");
+        System.out.println(" **     ** ");
+        System.out.println("   *****   ");
+    }
+
+    String dimensionString()
+    {
+        return String.valueOf((int)radius);
     }
 }
 
 class Square extends Shape {
-    ;
+    double length;
+
+    public Square(String name, double newLength)
+    {
+        super(name);
+        length = newLength;
+    }
+
+    double area()
+    {
+        return length *length;
+    }
+
+    void draw()
+    {
+        System.out.println("***");
+        System.out.println("* *");
+        System.out.println("***");
+    }
+
+    String dimensionString()
+    {
+        return String.valueOf((int)length);
+    }
+}
+
+class Triangle extends Shape {
+    double base, height;
+    
+    public Triangle(String name, double newBase, double newHeight)
+    {
+        super(name);
+        base = newBase;
+        height = newHeight;
+    }
+
+    double area()
+    {
+        return 0.5 * base * height;
+    }
+
+    void draw()
+    {
+        System.out.println("  *  ");
+        System.out.println(" * * ");
+        System.out.println("*****");
+    }
+
+    String dimensionString()
+    {
+        return ((int)base) + ", " + ((int)height);
+    }
+}
+
+class Rectangle extends Square{
+    double width, height;
+
+    public Rectangle(String name, double newWidth, double newHeight)
+    {
+        super(name, newHeight);
+        width = newWidth;
+    }
+
+    double area()
+    {
+        return width * height;
+    }
+
+    void draw()
+    {
+        System.out.println("*****");
+        System.out.println("*   *");
+        System.out.println("*   *");
+        System.out.println("*****");
+    }
+
+    String dimensionString()
+    {
+        return ((int)height) + ", "+ ((int)width);
+    }
 }
 
 
