2bd2dc8fb52eaf2a55b26abc9dd5b3e382d4f6ff
Luke Lu <chungl7@uci.edu>
2025-10-08 23:17:31 -0700

    correct bug

--- mainClass.java
+++ mainClass.java
@@ -193,41 +193,6 @@ class Picture
 
 
 
-
-class LinkedList {
-    ListNode head;
-
-    LinkedList(){
-        head = null;
-    }
-
-    void add (Shape x)
-    {
-        head = new ListNode(x, head);
-    }
-
-    int length()
-    {
-        int len = 0;
-        for(ListNode p = head; p != null; p = p.next)
-            ++len;
-        return len;
-    }
-
-    boolean isEmpty()
-    {
-        return head == null;
-    }
-
-    public String toString()
-    {
-        String result = "";
-        for(ListNode p = head; p != null; p = p.next)
-            result += p.info.toString() + " ";
-        return result;
-    }
-}
-
 public class mainClass
 {
     public static void main(String[] args)
