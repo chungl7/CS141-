c99d1c6459fc79560e176276b391a54044cfc49a
Luke Lu <chungl7@uci.edu>
2025-10-12 23:50:29 -0700

    first submission

---
+++ main.cpp
@@ -0,0 +1,262 @@
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+using namespace std;
+
+
+typedef double (*double_method_type)(void *);
+typedef void (*void_method_type)(void *);
+typedef std::string (*string_method_type)(void *);
+
+typedef union{
+    double_method_type double_method;
+    void_method_type void_method;
+    string_method_type string_method;
+} VirtualTableEntry;
+
+typedef VirtualTableEntry * VTableType;
+
+
+#define AREA_INDEX 0
+#define DRAW_INDEX 1
+#define DIMSTR_INDEX 2
+
+#define PI 3.14159
+
+struct Shape
+{
+    VTableType VPointer;
+    std::string name;
+};
+
+static inline void Shape_print(Shape* _this)
+{
+    std::string dims = _this->VPointer[DIMSTR_INDEX].string_method(_this);
+    double a = _this->VPointer[AREA_INDEX].double_method(_this);
+    cout << _this->name << "(" << dims << ") : " << a << "\n";
+}
+
+
+struct Circle
+{
+    VTableType VPointer;
+    std::string name;
+    double radius;
+};
+
+static double Circle_area(Circle* _this)
+{
+    return PI * _this->radius * _this->radius;
+}
+
+static void Circle_draw(Circle*_this)
+{
+    cout << "  ***  \n"
+            " *   * \n"
+            "  ***  \n";
+}
+
+static string Circle_dimensionString(Circle* _this)
+{
+    return to_string((int)_this->radius);
+}
+
+double Circle_circumference(Circle* _this)
+{
+    return 2.0 * PI *_this->radius;
+}
+
+VirtualTableEntry Circle_VTable [] = 
+{
+    {.double_method=(double_method_type)Circle_area},
+    {.double_method=(double_method_type)Circle_circumference},
+    {.string_method=(string_method_type)Circle_dimensionString}
+};
+
+Circle * Circle_Circle(Circle * _this, const string newName, double newRadius)
+{
+    _this->VPointer = Circle_VTable;
+    _this->name = newName;
+    _this->radius = newRadius;
+    return _this;
+}
+
+
+struct Square
+{
+    VTableType VPointer;
+    std::string name;
+    double length;
+};
+
+static double Square_area(Square* _this)
+{
+    return _this->length * _this->length;
+}
+
+static void Square_draw(Square* _this)
+{
+    cout << "*** \n"
+            "* * \n"
+            "*** \n";
+}
+
+static string Square_dimensionString(Square* _this)
+{
+    return to_string((int)_this->length);
+}
+
+static VirtualTableEntry Square_VTable[] = 
+{
+    {.double_method= (double_method_type) Square_area},
+    {.void_method= (void_method_type) Square_draw},
+    {.string_method= (string_method_type) Square_dimensionString}
+};
+
+static Square* Square_Square(Square* _this, const string newName, double newLength)
+{
+    _this->VPointer = Square_VTable;
+    _this->name = newName;
+    _this->length = newLength;
+    return _this;
+}
+
+
+struct Rectangle
+{
+    VTableType VPointer;
+    string name;
+    double width, height;
+};
+
+static double Rectangle_area(Rectangle* _this)
+{
+    return _this->width * _this->height;
+}
+
+static void Rectangle_draw(Rectangle* _this)
+{
+    cout << "***** \n"
+            "*   * \n"
+            "*   * \n"
+            "*   * \n"
+            "*   * \n"
+            "*   * \n"
+            "***** \n";
+}
+
+static string Rectangle_dimension(Rectangle* _this)
+{
+    return to_string((int)_this->width) + ", " + to_string((int)_this->height);
+}
+
+static VirtualTableEntry Rectangle_VTable[] = 
+{
+    {.double_method = (double_method_type) Rectangle_area},
+    {.void_method = (void_method_type) Rectangle_draw},
+    {.string_method = (string_method_type) Rectangle_dimension} 
+};
+
+static Rectangle* Rectangle_Rectangle(Rectangle*_this, const string newName, double newWidth, double newHeight)
+{
+    _this->VPointer = Rectangle_VTable;
+    _this->name = newName;
+    _this->width = newWidth;
+    _this->height = newHeight;
+    return _this;
+}
+
+
+struct Triangle
+{
+    VTableType VPointer;
+    string name;
+    double base;
+    double height;
+};
+
+static double Triangle_area(Triangle* _this)
+{
+    return 0.5 * _this->base * _this->height;
+}
+
+static void Triangle_draw(Triangle*_this)
+{
+    cout << "  *  \n"
+            " * * \n"
+            "***** \n";
+}
+
+static string Triangle_dimensionString(Triangle*_this)
+{
+    return to_string((int)_this->base) + ", " + to_string((int)_this->height);
+}
+
+static VirtualTableEntry Triangle_VTable[] = 
+{
+    {.double_method = (double_method_type) Triangle_area},
+    {.void_method = (void_method_type) Triangle_draw},
+    {.string_method = (string_method_type) Triangle_dimensionString}
+};
+
+static Triangle* Triangle_Triangle(Triangle* _this, const string newName, double newBase, double newHeight)
+{
+    _this->VPointer = Triangle_VTable;
+    _this->name = newName;
+    _this->base = newBase;
+    _this->height = newHeight;
+    return _this;
+}
+
+static void printAll(Shape** arr, int n)
+{
+    for (int i = 0; i < n; ++i) Shape_print(arr[i]);
+}
+
+static void drawAll(Shape** arr, int n)
+{
+    for (int i = 0; i < n; ++i) arr[i]->VPointer[DRAW_INDEX].void_method(arr[i]);
+}
+
+static double totalArea(Shape** arr, int n)
+{
+    double s = 0.0;
+    for (int i = 0; i < n; ++i)
+        s += arr[i]->VPointer[AREA_INDEX].double_method(arr[i]);
+    return s;
+}
+
+int main(int argc, char** argv)
+{
+    int value1 = atoi(argv[1]);
+    int value2 = atoi(argv[2]);
+
+    int value1_1 = value1-1;
+    int value2_2 = value2-1;
+
+    Triangle* t1 = Triangle_Triangle(new Triangle, "FirstTriangle", value1, value2);
+    Triangle* t2 = Triangle_Triangle(new Triangle, "SecondTriangle", value1_1, value2_2);
+
+    Circle* c1 = Circle_Circle(new Circle, "FirstCircle", value1);
+    Circle* c2 = Circle_Circle(new Circle, "SecondCircle", value1_1);
+
+    Square* s1 = Square_Square(new Square, "FirstSquare", value1);
+    Square* s2 = Square_Square(new Square, "SecondSquare", value1_1);
+
+    Rectangle* r1 = Rectangle_Rectangle(new Rectangle, "FirstRectangle", value1, value2);
+    Rectangle* r2 = Rectangle_Rectangle(new Rectangle, "SecondRectangle", value1_1, value2_2);
+
+    vector<Shape*> picture = 
+    {
+        (Shape*) r2, (Shape*) r1,
+        (Shape*) s2, (Shape*) s1,
+        (Shape*) c2, (Shape*) c1,
+        (Shape*) t2, (Shape*) t1
+    };
+
+    printAll(picture.data(), (int)picture.size());
+    drawAll(picture.data(), (int)picture.size());
+    cout << "Total : " << totalArea(picture.data(), (int)picture.size()) << "\n";
+    return 0;
+}
