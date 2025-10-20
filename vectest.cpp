f703584cf3f08afcc755b9367c37a2b28724d3d8
Luke Lu <chungl7@uci.edu>
2025-10-20 14:39:31 -0700

    first try

--- vectest.cpp
+++ vectest.cpp
@@ -1,4 +1,5 @@
 #include "vector.h"
+#include "cassert"
 
 int main() // Here is a start:
 
@@ -14,24 +15,66 @@ int main() // Here is a start:
 
         cout << "intVec" << intVec << endl;
 
-// "intVec(1, 3, 5, 7, 9)"
+        // "intVec(1, 3, 5, 7, 9)"
 
         cout << "iv" << iv << endl;
 
-// "iv(1, 3, 5, 7, 9)"
+        // "iv(1, 3, 5, 7, 9)"
 
         cout << "doubleVec" << doubleVec << endl;
 
-// "doubleVec(1.5, 2.5, 3.5, 4.5)"
+        // "doubleVec(1.5, 2.5, 3.5, 4.5)"
 
-cout << "dv" << dv << endl;
+        cout << "dv" << dv << endl;
 
 
-// "dv(1.5, 2.5, 3.5, 4.5)"
+        // "dv(1.5, 2.5, 3.5, 4.5)"
 
 
         // add at least one test case for each method defined in Vector
 
-return 0;
+        assert(doubleVec.size() == 4);
+        assert(intVec.size() == 5);
+        assert(dv.size() == 4);
+        assert(iv.size() == 5);
+
+        iv[0] = 10;
+        dv[0] = 10.0;
+        assert(iv[0] == 10);
+        assert(intVec[0] == 1);
+        assert(dv[0] == 10.0);
+        assert(doubleVec[0] == 1.5);
+
+        Vector<int>test1{1,2,3,4};
+        assert(intVec * test1 == 50);
+
+        Vector<int> a{1,2,3}, b{1,2,3,4};
+        Vector<int> c = a + b;
+        assert(c.size() == 4);
+        assert(c[0] == 2 && c[1] == 4 && c[2] == 6 && c[3] == 4);
+        Vector<double> a1{1.1, 2.2, 3.3}, b1{1.1};
+        Vector<double> c1 = a1 + b1;
+        assert(c1.size() == 3);
+        assert(c1[0] == 2.2 && c1[1] == 2.2 && c1[2] == 3.3);
+
+        Vector<int> test2 {0,1,2};
+        test2 = intVec;
+        assert(test2.size() == intVec.size());
+        test2[1] = 22;
+        assert(intVec[1] == 3);
+
+        assert(intVec != test2);
+        Vector<double> test3 {1.5,2.5,3.5,4.5};
+        assert(test3 == doubleVec);
+
+
+        Vector<int> scaled = 2 * test2;
+        Vector<double> shifted = 30 + test3;
+        assert(scaled[1] == 44);
+        assert(shifted[1] == 32.5);
+
+        cout << "All test done!";
+
+        return 0;
 
 }
--- vector.h
+++ vector.h
@@ -36,284 +36,150 @@ class Vector {
 
  private:
 
-  /**
-
-   * size_t sz   size of the vector i.e the amount of elements in the vector
-
-  */
-
   size_t sz;
-
-
-  /**
-
-   * T* buf  pointer to the base (first element) of a dynamically allocated array
-
-   *    Be careful to manage its memory wisely (calling del[] when necessary)
-
-  */
-
-  T* buf;  
+  T* buf;
 
 
  public:
+    Vector(size_t newSz)
+      : sz(newSz), buf(sz ? new T[sz]{} : nullptr) {}
 
-  /**
+    ~Vector() {delete[] buf;}
 
-   * Constructs a vector of size sz
+    Vector(initializer_list<T> L)
+      :sz(L.size()), buf(sz ? new T[sz]: nullptr)
+      {
+        size_t i = 0;
+        for (const T& x: L) buf[i++] = x;
+      }
 
-   *
 
-   * ex: Vector v(10); -> constructs a 10 elem Vector
+  Vector(const Vector & v) 
+    :sz(v.sz), buf(sz ? new T[sz]: nullptr)
+    {
+      for(size_t i = 0; i < sz; ++i)
+      {
+        buf[i] = v.buf[i];
+      }
+    }
 
-   * @param sz size of vector
 
-  */
+  size_t size() const 
+  {
+    return sz;
+  }
 
-  Vector(size_t sz) {}
 
+  T & operator [] (const int i) 
+  {
+    if (i < 0 || static_cast<size_t>(i) >= sz)
+      throw out_of_range("The index is out of bounds \n");
+    return buf[i];
+  }
 
 
-  /**
 
-   * Constructs a vector from a list of elements
-
-   *
-
-   * ex: Vector v1{1, 2, 3}; -> creates a vector with values 1, 2, 3 and size 3
-
-   * @param L a list of values to initialize our vector
-
-   *   - L.size() gives the list's size
-
-   *   - You can iterate through it using an iterator
-
-  */
-
-  Vector(initializer_list<T> L) {}
-
-
-
-/**
-
-   * Destructs the object at the end of the object's lifecycle
-
-   *  - Automatically called
-
-   * Deallocate the array here.
-
-   * Some versions of valgrind report 72704 bytes in one still-reachable block.  
-
-   * You can ignore that.
-
-  */
-
-  ~Vector() {}
-
-
-
-  /**
-
-   * Copy constructor; makes a new Vector by deep copying the vector passed to it
-
-   * ex: Vector v2{v1};
-
-  */
-
-  Vector(const Vector & v) {}
-
-
-
-  /**
-
-   * Returns the size of the vector
-
-   * ex: Vector v1(10); v1.size(); -> will return 10
-
-   * @return size of vector
-
-  */
-
-  size_t size() const {}
-
-
-
-  /**
-
-  * Overloads the [] operator and returns a reference to the value at index i in the
-
-  * dynamically allocated array. This would be used to change the value at that index.
-
-  * Throws an error when accessing index out of bounds
-
-  * ex: v1[2] = 3;
-
-  * @param i index of elem in buf that will be accessed
-
-  */
-
-  T & operator [] (const int i) {}
-
-
-
-  /**
-
-  * Overloads the [] operator and returns the value of the elem at index i in the
-
-  * dynamically allocated array. This would be used to access the value at that index
-
-  * without modifying it.
-
-  * Throws an error when accessing index out of bounds
-
-  * ex: T elemAtInd3 = v1[3];
-
-  * @param i index of elem in buf that will be accessed
-
-  */
-
-  T operator [] (const int i) const {}
+  T operator [] (const int i) const 
+  {
+    if (i < 0 || static_cast<size_t>(i) >= sz)
+      throw out_of_range("The index is out of bounds \n");
+    return buf[i];
+  }
 
  
 
 
-  /**
+  T operator * (const Vector & v) const 
+  {
+    if(sz == 0 || v.sz == 0) return T{};
 
-   * Dot products the current vector with the passed vector.
+    const size_t temp = min(sz, v.sz);
 
-   * The dot product of two vectors is the sum of the products
+    T return_value{};
 
-   * of the corresponding entries of two sequences of numbers.
+    for(size_t i = 0; i < temp; ++i)
+    {
+      return_value += buf[i] * v.buf[i];
+    }
+    return return_value;
+  }
 
-   *
 
-   * ex: T x = V1 * V2;
 
-   * dot product: [1, 2] * [3, 4, 5] = 1 * 3 + 2 * 4 + 0 = 11
+  Vector operator + (const Vector & v) const 
+  {
+    const size_t temp = max(sz, v.sz);
 
-   * Assume an empty Vector will cause the product to be 0.
+    Vector return_list(temp);
 
-   * @param v Vector on the right to dot product with
+    const size_t share_length = min(sz, v.sz);
 
-   * @return a scalar value with type T (not a vector!) that is the dot product of the
+    for(size_t i = 0; i < share_length; ++i)
+    {
+      return_list.buf[i] = buf[i] + v.buf[i];
+    }
 
-   *    two vectors
+    if(sz < v.sz)
+    {
+      for (size_t i = share_length; i < temp; ++i) return_list.buf[i] = v.buf[i];
+    }
+    else if (sz > v.sz)
+    {
+      for(size_t i  = share_length; i < temp; ++i) return_list.buf[i] = buf[i];
+    }
+    return return_list;
 
-  */
+  }
 
-  T operator * (const Vector & v) const {}
 
+  const Vector & operator = (const Vector & v) 
+  {
+    if (this == &v) return *this;
 
+    T* newbuf = (v.sz ? new T[v.sz]: nullptr);
+    for(size_t i = 0; i < v.sz; ++i) 
+    {
+      newbuf[i] = v.buf[i];
+    }
+    delete [] buf;
+    buf = newbuf;
+    sz = v.sz;
+    return *this;
+  }
 
-  /**
 
-   * Adds the current vector with the passed vector and returns a new vector.
+  bool operator == (const Vector & v) const 
+  {
+    if (v.sz != sz) return false;
+    for (size_t i = 0; i < sz; ++i)
+    {
+      if(!(buf[i] == v.buf[i])) return false;
+    }
+    return true;
+  }
 
-   * ex: V3 = V1 + V2;
 
-   * addition: [1, 2, 3] + [4, 5, 6, 7] = [5, 7, 9, 7]
 
-   * @param v Vector on the right to perform addition with
+  bool operator != (const Vector & v) const 
+  {
+    return !(*this == v);
+  }
 
-   * @return new vector where index i is the result of this[i] + v[i]
 
-  */
+  inline friend Vector operator * (const int scale, const Vector & v)
+  {
+    Vector return_list(v.sz);
+    for(size_t i = 0; i < v.sz; ++i) return_list.buf[i] = static_cast<T>(scale) * v.buf[i];
+    return return_list;
+  }
 
-  Vector operator + (const Vector & v) const {}
 
-
-
-  /**
-
-   * Destructs the current vector and deep copies the passed vector
-
-   * ex: V1 = V2;
-
-   * V1 could be an already existing vector, be sure to clean it up before the deep copy
-
-   * @param v Vector on the right to deep copy
-
-   * @return reference to the current object
-
-  */
-
-  const Vector & operator = (const Vector & v) {}
-
- 
-
-
-  /**
-
-   * Determines whether the current vector is equivalent to the passed vector
-
-   * ex: bool isV1AndV2Same = V1 == V2;
-
-   * @param v Vector on the right to compare current with
-
-   * @return true if both vectors are deeply equivalent (elem by elem comparison)
-
-   * and false otherwise
-
-  */
-
-  bool operator == (const Vector & v) const {}
-
-
-
-  /**
-
-   * Determines whether the current vector is not equivalent to the passed vector
-
-   * ex: bool isV1AndV2Different = V1 != V2;
-
-   * @param v Vector on the right to compare current with
-
-   * @return false if both vectors are deeply equivalent (elem by elem comparison)
-
-   * and true otherwise
-
-  */
-
-  bool operator != (const Vector & v) const {}
-
-
-  /**
-
-   * Multiplies each element in current vector with the passed integer and returns a new vector.
-
-   * ex: V1 = 20 * V2; it is important that 20 is on the left!
-
-   * 20 * [1, 2, 3] = [20, 40, 60]
-
-   * @param scale integer to multiple each element of vector v
-
-   * @param v Vector on the right to perform multiplication on
-
-   * @return new vector where index i is the result of v[i] * scale
-
-  */
-
-  inline friend Vector operator * (const int scale, const Vector & v) {}
-
-
-
-  /**
-
-   * Adds each element in the current vector with the passed integer and returns a new vector.
-
-   * ex: V1 = 20 + V2; it is important that 20 is on the left!
-
-   * 20 + [1, 2, 3] = [21, 22, 23]
-
-   * @param adder integer to add to each element of vector v
-
-   * @param v Vector on the right to perform addition on
-
-   * @return new vector where index i is the result of v[i] + adder
-
-  */
-
-  inline friend Vector operator + (const int adder, const Vector & v) {}
+  inline friend Vector operator + (const int adder, const Vector & v)
+  {
+    Vector return_list(v.sz);
+    for (size_t i = 0; i < v.sz; ++i) return_list.buf[i] = static_cast<T>(adder) + v.buf[i];
+    return return_list;
+  }
 
 
   /**
@@ -330,7 +196,17 @@ class Vector {
 
   */
 
-  inline friend ostream& operator << (ostream & o, const Vector & v) {}
+  inline friend ostream& operator << (ostream & o, const Vector & v)
+  {
+    o << "(";
+    for (size_t i = 0; i < v.sz; ++i)
+    {
+      o << v.buf[i];
+      if(i+1 < v.sz) o << ", ";
+    }
+    o << ")";
+    return o;
+  }
 
  
 
